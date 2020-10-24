c13d315abaf0bfa2cd2df4649b53722b87a2a054
Fernando Sitoe <44120880+profnatoe@users.noreply.github.com>
2020-10-24 02:10:51 +0200

    Updated Vectors, All implementations included

--- Task 16/vectors.py
+++ Task 16/vectors.py
@@ -14,71 +14,38 @@
 # Your definition should include a constructor,  __str__, values for the x and y
 # coordinates, and methods for addition and subtraction of vectors, scalar multiplication,
 # length, and comparison.
-
 import math
 
-class Vector(object):
-    def __init__(self, *args):
-        """ Create a vector, example: v = Vector(1,2) """
-        if len(args)==0: self.values = (0,0)
-        else: self.values = args
-        
-        
-    def matrix_mult(self, matrix):
-        """ Multiply this vector by a matrix.  Assuming matrix is a list of lists.
-        
-            Example:
-            mat = [[1,2,3],[-1,0,1],[3,4,5]]
-            Vector(1,2,3).matrix_mult(mat) ->  (14, 2, 26)
-         
-        """
-        if not all(len(row) == len(self) for row in matrix):
-            raise ValueError('Matrix must match vector dimensions') 
-        
-        # Grab a row from the matrix, make it a Vector, take the dot product, 
-        # and store it as the first component
-        product = tuple(Vector(*row)*self for row in matrix)
-        
-        return Vector(*product)
+class Vectors(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
     
-    def inner(self, other):
-        """ Returns the dot product (inner product) of self and other vector
-        """
-        return sum(a * b for a, b in zip(self, other))
-    
-    def __mul__(self, other):
-        """ Returns the dot product of self and other if multiplied
-            by another Vector.  If multiplied by an int or float,
-            multiplies each component by other.
-        """
-        if type(other) == type(self):
-            return self.inner(other)
-        elif type(other) == type(1) or type(other) == type(1.0):
-            product = tuple( a * other for a in self )
-            return Vector(*product)
-    
+    #addition
     def __add__(self, other):
-        """ Returns the vector addition of self and other """
-        added = tuple( a + b for a, b in zip(self, other) )
-        return Vector(*added)
-    
-    def __sub__(self, other):
-        """ Returns the vector difference of self and other """
-        subbed = tuple( a - b for a, b in zip(self, other) )
-        return Vector(*subbed)
-    
-    def __iter__(self):
-        return self.values.__iter__()
-    
-    def __len__(self):
-        return len(self.values)
-    
-    def __getitem__(self, key):
-        return self.values[key]
-        
-    def __repr__(self):
-        return str(self.values)
+        return Vectors(self.x + other.x, self.y + other.y)
 
-   
+    #subtraction
+    def __sub__(self, other):
+        return Vectors(self.x - other.x, self.y - other.y)
+    
+    #multiplication
+    def __mul__(self, other):
+        return self.x*other.x + self.y*other.y
+
+    def __abs__(self):
+        return math.sqrt(self.x**2 + self.y**2)
+    
+    #length, will always be two for a valid vector in this project
+    def __len__(self):
+        return 2
+    
+    #checks for comparison    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+    #our toString
+    def __str__(self):
+        return '(%g, %g)' % (self.x, self.y)
 
    
