23bcd530e582ab02770f1a78c11227ee32bc5802
Fernando Sitoe <44120880+profnatoe@users.noreply.github.com>
2020-10-29 21:58:30 +0200

    Updated movies.py

--- Task 19/movies.py
+++ Task 19/movies.py
@@ -1,36 +1,35 @@
-
 import spacy
 nlp = spacy.load("en_core_web_md")
 
-#movie_titles: ["Movie A", "Movie B", "Movie C", "Movie D", "Movie E", "Movie F", "Movie G", "Movie H", "Movie I", "Movie J"]
+movies = []
+similarities = []
+msDictionary = {}
 
-movies = [
-    'Movie A :When Hiccup discovers Toothless isn\'t the only Night Fury, he must seek "The Hidden World", a secret Dragon Utopia before a hired tyrant named Grimmel finds it first.',
-    'Movie B :After the death of Superman, several new people present themselves as possible successors.',
-    'Movie C :A darkness swirls at the center of a world-renowned dance company, one that will engulf the artistic director, an ambitious young dancer, and a grieving psychotherapist. Some will succumb to the nightmare. Others will finally wake up.',
-    'Movie D :A humorous take on Sir Arthur Conan Doyle\'s classic mysteries featuring Sherlock Holmes and Doctor Watson.',
-    'Movie E :A 16-year-old girl and her extended family are left reeling after her calculating grandmother unveils an array of secrets on her deathbed.',
-    'Movie F :In the last moments of World War II, a young German soldier fighting for survival finds a Nazi captain\'s uniform. Impersonating an officer, the man quickly takes on the monstrous identity of the perpetrators he is trying to escape from.',
-    'Movie G :The world at an end, a dying mother sends her young son on a quest to find the place that grants wishes.',
-    'Movie H :A musician helps a young singer and actress find fame, even as age and alcoholism send his own career into a downward spiral.',
-    'Movie I :Corporate analyst and single mom, Jen, tackles Christmas with a business-like approach until her uncle arrives with a handsome stranger in tow.',
-    'Movie J :Adapted from the bestselling novel by Madeleine St John, Ladies in Black is an alluring and tender-hearted comedy drama about the lives of a group of department store employees in 1959 Sydney.',
-]
+with open("movies.txt") as file_in:
+    for line in file_in:
+        movies.append(line)
 
 sentence_to_compare = "Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator"
 
 def NextMovie(description):
-    similarities = []
-    detailed = []
+    print("Sentence to Compare with: \n=====\n")
+    print(description)
+    print("\n=============\n")
     model_sentences = nlp(description)
     for movie in movies:
         similarity = nlp(movie).similarity(model_sentences)
-        #print(movie + "-" + str(similarity))
         similarities.append(similarity)
-        detailed.append(str(movie) + " -- " + str(similarity))
-    print(max(similarities))
-    if(max(similarities) in detailed):
-        return detailed
+        
+        msDictionary[similarity] = movie
+
+    #print max similarity value just to compare with the dictionary : Debugging
+    
+    print("\nMax Similarity Value: \n", str(max(msDictionary)))
+    print("\n\nMovie-Similarity Dictionary with Similarity as the Key:\n=====\n\n")
+    for key,value in msDictionary.items():
+        print(key, ' : ', value)
+
+    return (msDictionary[max(similarities)])
 
 
-print(NextMovie(sentence_to_compare))
+print("================\n\nNext Movie to Watch is: ", NextMovie((sentence_to_compare)))
